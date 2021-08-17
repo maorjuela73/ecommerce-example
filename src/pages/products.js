bd5a0386f5333c08fe3c4f0e5bd4c4715ac4381e
Miguel Orjuela <ma.orjuela73@gmail.com>
2021-08-16 19:49:45 -0500

    E-commerce front finished

---
+++ src/pages/products.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: '1',
+        name: 'Aceite de Linaza',
+        price: '$10.000',
+        description: '30 ml aceite orgánico natural prensado en frio',
+    },
+    {
+        id: '2',
+        name: 'Aceite de Ajonjolí Crudo',
+        price: '$10.000',
+        description: '30 ml aceite orgánico natural prensado en frio',
+    },
+    {
+        id: '3',
+        name: 'Aceite de Girasol',
+        price: '$10.000',
+        description: '30 ml aceite orgánico natural prensado en frio',
+    },
+    {
+        id: '4',
+        name: 'Aceite de Almendras',
+        price: '$10.000',
+        description: '30 ml aceite orgánico natural prensado en frio',
+    },
+];
