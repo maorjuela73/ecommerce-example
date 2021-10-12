0cc972308e57a0a2a312b7b310726a2d845d2ef3
Miguel Orjuela <ma.orjuela73@gmail.com>
2021-10-12 18:49:50 -0500

    Employee of the month added

---
+++ src/pages/EmployeeOfTheMonthPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>Employee Of The Month</h1>
+    <h3>{{personData.name.first}} {{personData.name.last}} </h3>
+    <p></p>
+</template>
+
+<script>
+export default {
+    name: 'EmployeeOfTheMonthPage',
+    data() {
+        return {
+            personData: null,
+        }
+    },
+    created() {
+        fetch('https://randomuser.me/api/')
+            .then(response => response.json())
+            .then(data => this.personData = data.results[0])
+    },
+}
+</script>
+
